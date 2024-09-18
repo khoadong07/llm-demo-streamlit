53bf54ecab278326643d94fe985f7079c8a19987
khoadong07 <khoadong07@gmail.com>
2024-09-18 15:47:01 +0700

    new proj

---
+++ app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import time
+import pandas as pd
+from deep_infa import call_deep_infra
+
+def process_call_llm(text):
+    result_llm, input_token, output_token = call_deep_infra(text)
+    return result_llm, input_token, output_token
+
+def normalize_field_name(field):
+    field_map = {
+        "sentiment": "Sentiment",
+        "severity": "Severity",
+        "emotion": "Emotion",
+        "polarity": "Polarity",
+        "intensity": "Intensity",
+        "category": "Category",
+        "industry": "Industry",
+        "angle": "Angle",
+        "intent": "Intent",
+        "purpose": "Purpose",
+        "tone": "Tone",
+        "brand_attribute": "Brand Attribute",
+        "spam": "Spam",
+        "advertisement": "Advertisement",
+        "opinion_expressed": "Opinion Expressed",
+        "emotional_tone": "Emotional Tone",
+        "feedback_provided": "Feedback Provided",
+        "insight_provided": "Insight Provided"
+    }
+    return field_map.get(field, field)
+
+st.title('LLM Demo API')
+
+input_text = st.text_area("Enter your text here:")
+
+if st.button('Run'):
+    if input_text.strip() == "":
+        st.warning("Please input some text before running.")
+    else:
+        st.subheader("Analysis Result")
+        with st.spinner('Processing...'):
+            start_time = time.time()
+
+            result_llm, input_token, output_token = process_call_llm(input_text)
+
+            end_time = time.time()
+            processing_time = end_time - start_time
+
+        st.write(f"Processing time: {processing_time:.2f} seconds")
+        st.write(f"Input token: {input_token} tokens")
+        st.write(f"Output token: {output_token} tokens")
+        if isinstance(result_llm, list) and len(result_llm) > 0:
+            result_llm = result_llm[0]
+
+        if isinstance(result_llm, dict):
+            data = {
+                "Field": [normalize_field_name(k) for k in result_llm.keys()],
+                "Value": [str(v) for v in result_llm.values()]
+            }
+
+            df = pd.DataFrame(data)
+            st.table(df)
+
+            st.json(result_llm)
+        else:
+            st.error("Unexpected result format.")
---
+++ deep_infa.py
@@ -0,0 +1,54 @@
+import requests
+import json
+import re
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+deep_infra_key = os.getenv('DEEP_INFRA_KEY')
+deep_infra_url = os.getenv('DEEP_INFRA_URL')
+
+openai = OpenAI(
+    api_key=deep_infra_key,
+    base_url=deep_infra_url,
+)
+
+
+def load_prompt_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+
+def call_deep_infra(raw_content):
+    chat_completion = openai.chat.completions.create(
+        model="meta-llama/Meta-Llama-3.1-8B-Instruct",
+        messages=[
+        {
+          "role": "user",
+          "content": load_prompt_file("prompt.txt")
+        },
+
+          {
+              "role": "user",
+              "content": f"With the provided knowledge, please process the following content {raw_content}. The result should be formatted as JSON with field names and corresponding values."
+          }
+      ])
+
+    result_llm = chat_completion.choices[0].message.content
+    print(result_llm)
+    result_llm = parse_custom_json_string(result_llm)
+    prompt_token = chat_completion.usage.prompt_tokens
+    output_token = chat_completion.usage.completion_tokens
+
+    return result_llm, prompt_token, output_token
+
+
+def parse_custom_json_string(custom_string):
+    try:
+        json_data = re.search(r'```(.*?)```', custom_string, re.DOTALL).group(1).strip()
+        parsed_result = json.loads(json_data)
+        return parsed_result
+    except (json.JSONDecodeError, AttributeError) as e:
+        return f"Failed to parse JSON: {e}"
+
